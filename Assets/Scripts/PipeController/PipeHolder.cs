73a23ad85863af21dce3f025abe7115bf1fe3c0a
The Boss <tatchu.it@gmail.com>
2017-12-23 11:54:44 +0700

    add trigger destroy pipe holder

---
+++ Assets/Scripts/PipeController/PipeHolder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeHolder : MonoBehaviour {
+
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_PipeMove ();
+	}
+
+	void _PipeMove(){
+		Vector3 temp = transform.position;
+		temp.x -= speed * Time.deltaTime;
+		transform.position = temp;
+	}
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.tag == "destroy_pipe") {
+			Destroy (gameObject);
+		}
+	}
+}
