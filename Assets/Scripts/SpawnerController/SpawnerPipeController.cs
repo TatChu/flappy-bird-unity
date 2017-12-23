d97918b390089ce31ca292696d4bae4c2c8f7f7a
The Boss <tatchu.it@gmail.com>
2017-12-23 11:34:25 +0700

    update spawner pipe

---
+++ Assets/Scripts/SpawnerController/SpawnerPipeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerPipeController : MonoBehaviour {
+
+
+	[SerializeField]
+	private GameObject pipeHolder;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Spawner());
+	}
+	
+	IEnumerator Spawner(){
+		yield return new WaitForSeconds (3f);
+		Vector3 pipePosition = transform.position;
+		pipePosition.y = Random.Range(-2.6f, 2.6f);
+		Instantiate (pipeHolder, pipePosition, Quaternion.identity);
+		StartCoroutine (Spawner ());
+	}
+}
